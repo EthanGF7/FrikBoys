85f25b82850567b3566153d7b23671035647241b
David Hernández Giménez <davidhernandez@ceroca.cat>
2025-11-19 20:14:29 +0100

    Arreglo
    separo de PagMarvel el css

--- public/JS/Productos.js
+++ public/JS/Productos.js
@@ -5,8 +5,8 @@ export const productos = [
     nombre: "Reactor Arc – Corazón de Iron Man",
     precio: 84.99,
     modelo: "../Models3d/Marvel/corazonIronman.glb",
+    imagen: "../Models3d/Marvel/preview_corazon.jpg",
     descripcion: "Réplica exacta del reactor de arco que mantiene con vida a Tony Stark. Impreso en 3D con detalle extremo y acabado metálico brillante."
   },
- 
 ];
 
